ede7d094277c42a2d4daef164833296f5a457490
Prince Saini <prince1191.be25@chitkarauniversity.edu.in>
2026-05-07 23:24:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,16 @@ const exchangeRates = {
   USD: 1,
   INR: 83,
   EUR: 0.92,
-  GBP: 0.79
+  GBP: 0.79,
+  JPY: 157,
+  CNY: 7.2,
+  AED: 3.67,
+  SAR: 3.75,
+  CAD: 1.37,
+  AUD: 1.39,
+  SGD: 1.27,
+  RUB: 75,
+  KRW:1370
 };
 
 convertBtn.addEventListener("click", () => {
